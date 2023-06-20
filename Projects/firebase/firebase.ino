c4f31920088e76306cb1213cf31d90055c3a2d6f
Thiago S. Marcelino <thiagodsmarcelino@gmail.com>
2023-06-20 16:51:52 -0300

    Merging projects

--- Projects/firebase/firebase.ino
+++ Projects/firebase/firebase.ino
@@ -1,12 +1,16 @@
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
+#include <TimeLib.h>
+#include <WiFi.h>
 
-#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+#define WIFI_SSID "iPhone de Maite"
+#define WIFI_PASSWORD "maite2302"
 
-#define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
-#define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" 
+#define API_KEY "AIzaSyDnzcrPDg26EvPpAc6wGhME3StJbx1qttg"
+#define DATABASE_URL "https://iot-senai-68871-default-rtdb.firebaseio.com/" 
+
+#define POT_PIN 25
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -14,30 +18,29 @@ FirebaseConfig config;
 
 bool signupOK = false;
 
-void setup(){
+void setup() {
   Serial.begin(115200);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
-  
+
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
-
+  
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
   if (Firebase.signUp(&config, &auth, "", "")) {
     Serial.println("Ok");
     signupOK = true;
-  }
-  else {
+  } else {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
 
@@ -47,11 +50,12 @@ void setup(){
   Firebase.reconnectWiFi(true);
 }
 
-void loop(){
+void loop(){    
   if (Firebase.ready() && signupOK) {
-
-    if (!Firebase.RTDB.setString(&fbdo, "Test", "Testing")) {
+    if (!Firebase.RTDB.setInt(&fbdo, String("LDR_Data/") + String(now()), analogRead(POT_PIN))) {
       Serial.println("ERRO: " + fbdo.errorReason());
     }
+
+    delay(10 * 1000);
   }
 }
